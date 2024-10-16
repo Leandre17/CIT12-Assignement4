b7be2d9900010f29f9668d2b1ccc294b745b97bc
Leandre17 <leandre.godet@epitech.eu>
2024-10-16 10:34:21 +0200

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿
+using Npgsql;
+
+
+
+var connectionString = "host=localhost;db=northwind;uid=postgres;pwd=";
+
+
+using var connection = new NpgsqlConnection(connectionString);
+connection.Open();
+
+using var cmd = new NpgsqlCommand();
+
+cmd.Connection = connection;
+cmd.CommandText = "select * from categories";
+
+using var reader = cmd.ExecuteReader();
+
+while(reader.Read())
+{
+    Console.WriteLine(reader.GetInt32(0));
+}
