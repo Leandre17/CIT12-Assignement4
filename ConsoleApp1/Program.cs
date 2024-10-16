549137af822a2ee5f959261fafdac4a555359621
Leandre17 <leandre.godet@epitech.eu>
2024-10-16 10:55:12 +0200

    first commit

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,5 @@
 ﻿
+using System.Data;
 using Npgsql;
 
 
@@ -9,14 +10,135 @@ var connectionString = "host=localhost;db=northwind;uid=postgres;pwd=";
 using var connection = new NpgsqlConnection(connectionString);
 connection.Open();
 
-using var cmd = new NpgsqlCommand();
 
-cmd.Connection = connection;
-cmd.CommandText = "select * from categories";
+// cmd.Connection = connection;
+// cmd.CommandText = "select * from categories";
 
-using var reader = cmd.ExecuteReader();
+// using var reader = cmd.ExecuteReader();
 
-while(reader.Read())
+// while(reader.Read())
+// {
+//     Console.WriteLine(reader.GetString(2));
+// }
+
+void Test()
 {
-    Console.WriteLine(reader.GetInt32(0));
+    using var cmd = new NpgsqlCommand();
+    cmd.Connection = connection;
+    cmd.CommandText = "select * from orders";
+    using var reader = cmd.ExecuteReader();
+    while (reader.Read())
+    {
+        try
+        {
+            Console.Write(reader.GetInt32(0) + " ");
+        }
+        catch (Exception e)
+        {
+            Console.Error.Write(e.Message);
+            Console.Write("null ");
+        }
+        try
+        {
+            Console.Write(reader.GetString(1) + " ");
+        }
+        catch (Exception e)
+        {
+            Console.Error.Write(e.Message);
+            Console.Write("null ");
+        }
+        try
+        {
+            Console.Write(reader.GetInt32(2) + " ");
+        }
+        catch (Exception e)
+        {
+            Console.Error.Write(e.Message);
+            Console.Write("null ");
+        }
+        try
+        {
+            Console.Write(reader.GetDateTime(3) + " ");
+        }
+        catch (Exception e)
+        {
+            Console.Error.Write(e.Message);
+            Console.Write("null ");
+        }
+        try
+        {
+            Console.Write(reader.GetDateTime(4) + " ");
+        }
+        catch (Exception e)
+        {
+            Console.Error.Write(e.Message);
+            Console.Write("null ");
+        }
+        try
+        {
+            Console.Write(reader.GetDateTime(5) + " ");
+        }
+        catch (Exception e)
+        {
+            Console.Error.Write(e.Message);
+            Console.Write("null ");
+        }
+        try
+        {
+            Console.Write(reader.GetInt32(6) + " ");
+        }
+        catch (Exception e)
+        {
+            Console.Error.Write(e.Message);
+            Console.Write("null ");
+        }
+        try
+        {
+            Console.Write(reader.GetString(7) + " ");
+        }
+        catch (Exception e)
+        {
+            Console.Error.Write(e.Message);
+            Console.Write("null ");
+        }
+        try
+        {
+            Console.Write(reader.GetString(8) + " ");
+        }
+        catch (Exception e)
+        {
+            Console.Error.Write(e.Message);
+            Console.Write("null ");
+        }
+        try
+        {
+            Console.Write(reader.GetString(9) + " ");
+        }
+        catch (Exception e)
+        {
+            Console.Error.Write(e.Message);
+            Console.Write("null ");
+        }
+        try
+        {
+            Console.Write(reader.GetString(10) + " ");
+        }
+        catch (Exception e)
+        {
+            Console.Error.Write(e.Message);
+            Console.Write("null ");
+        }
+        try
+        {
+            Console.Write(reader.GetString(11));
+        }
+        catch (Exception e)
+        {
+            Console.Error.Write(e.Message);
+            Console.Write("null");
+        }
+        Console.Write("\n");
+    }
 }
+
+Test();
